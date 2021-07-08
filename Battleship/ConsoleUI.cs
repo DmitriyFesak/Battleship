dfe77e3564bfc55c273d77b0129db809905c955f
Дмитро Фесак <Fesak.D.V@nmu.one>
2021-07-08 09:45:54 +0300

    Separated business logic from UI (different libraries)

--- Battleship/ConsoleUI.cs
+++ Battleship/ConsoleUI.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Battleship;
 
-namespace Battleship
+namespace BattleshipConsoleUI
 {
     public class ConsoleUI : IUserInterface
     {
